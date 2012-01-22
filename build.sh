3680aea5547180a3a543378423ac02ac7c76311c
Marton Juhasz <m@juhaszmarton.hu>
2012-01-22 15:45:25 +0100

    apache config, filezilla

--- build.sh
+++ build.sh
@@ -6,18 +6,18 @@ if [[ `whoami` != 'root' ]]; then
 fi
 
 # remove
-apt-get -yq remove audacious                                                                           \
-                   ace-of-penguins                                                                     \
-                   audacious-plugins                                                                   \
-                   libaudclient2                                                                       \
-                   libaudcore1                                                                         \
-                   transmission                                                                        \
-                   transmission-gtk                                                                    \
-                   guvcview                                                                            \
-                   transmission-common                                                                 \
-                   simple-scan                                                                         \
-                   xpad                                                                                \
-                   xfburn                                                                              \
+apt-get -yq remove audacious           \
+                   ace-of-penguins     \
+                   audacious-plugins   \
+                   libaudclient2       \
+                   libaudcore1         \
+                   transmission        \
+                   transmission-gtk    \
+                   guvcview            \
+                   transmission-common \
+                   simple-scan         \
+                   xpad                \
+                   xfburn              \
 
 # upgrade
 apt-get -yq update
@@ -35,25 +35,26 @@ echo phpmyadmin       phpmyadmin/setup-password         password root    | debco
 echo phpmyadmin       phpmyadmin/mysql/app-pass         password root    | debconf-set-selections
 
 # install
-apt-get -yq install git                                                                                \
-                    git-gui                                                                            \
-                    git-completion                                                                     \
-                    bind9                                                                              \
-                    sysv-rc-conf                                                                       \
-                    apache2                                                                            \
-                    apache2-threaded-dev                                                               \
-                    mysql-server                                                                       \
-                    phpmyadmin                                                                         \
-                    php5                                                                               \
-                    php5-cli                                                                           \
-                    php5-curl                                                                          \
-                    php5-dev                                                                           \
-                    php5-gd                                                                            \
-                    php5-mysql                                                                         \
-                    php5-sqlite                                                                        \
-                    php5-xdebug                                                                        \
-                    openssh-server                                                                     \
-                    vim-gnome                                                                          \
+apt-get -yq install git                  \
+                    git-gui              \
+                    git-completion       \
+                    bind9                \
+                    sysv-rc-conf         \
+                    apache2              \
+                    apache2-threaded-dev \
+                    mysql-server         \
+                    phpmyadmin           \
+                    php5                 \
+                    php5-cli             \
+                    php5-curl            \
+                    php5-dev             \
+                    php5-gd              \
+                    php5-mysql           \
+                    php5-sqlite          \
+                    php5-xdebug          \
+                    openssh-server       \
+                    vim-gnome            \
+                    filezilla            \
 
 # cleanup
 apt-get -yq autoremove
@@ -63,8 +64,14 @@ apt-get -yq clean
 echo -e "\nzone \"dev\" { type master; file \"/etc/bind/dev.zone\"; };\n" >> /etc/bind/named.conf.local
 wget --no-check-certificate -O /etc/bind/dev.zone https://raw.github.com/juhasz/dClass-builder/master/dev.zone
 
+# configure apache
+rm /etc/apache2/sites-availabe/default
+wget --no-check-certificate -O /etc/apache2/sites-availabe/default https://raw.github.com/juhasz/dClass-builder/master/apache-default-site
+a2enmod rewrite
+
 #restart services
 service bind9 restart
 service apache2 restart
+servere mysql restart
 
 exit 0
